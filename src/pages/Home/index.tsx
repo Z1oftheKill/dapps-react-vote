071bcf9fab100e31676c706a6f9a57d558d9f141
Z1oftheKill <ykwhb5210@163.com>
2024-06-14 07:37:36 +0800

    feat: 添加合约

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,59 +1,79 @@
 import React, { useEffect, useState } from 'react'
+import { Radio, Button } from 'antd'
 import { ethers } from 'ethers'
-const Home: React.FC = () => {
-  const [voteList, setVoteList] = useState([])
-  const getVoteList = async () => {
-    const provider = new ethers.JsonRpcProvider('http://0.0.0.0:8545')
-    const contractAddresss = '0xd2Ca10E0C912C635d46DEfAc13E0192159699dA7'
-    const abi = [
-      {
-        constant: true,
-        inputs: [],
-        name: 'getVoteList',
-        outputs: [
-          {
-            components: [
-              {
-                internalType: 'bytes32',
-                name: 'name',
-                type: 'bytes32'
-              },
-              {
-                internalType: 'uint256',
-                name: 'voteCount',
-                type: 'uint256'
-              }
-            ],
-            internalType: 'struct Ballot.Proposal[]',
-            name: '',
-            type: 'tuple[]'
-          }
-        ],
-        payable: false,
-        stateMutability: 'view',
-        type: 'function'
-      }
-    ]
-    const contract = new ethers.Contract(contractAddresss, abi, provider)
-    console.log('🚀 ~ getVoteList ~ contract:', contract)
-    let voteList = await contract.getVoteList()
-    voteList = [...voteList].map((i) => i[0])
-    voteList = voteList.map((i: string) => ethers.decodeBytes32String(i))
-    setVoteList(voteList)
 
-    console.log('🚀 ~ getVoteList ~ voteList:', voteList)
+import type { RadioChangeEvent } from 'antd'
+import { VoteListProp } from './interface'
+
+const Home: React.FC = () => {
+  const [voteList, setVoteList] = useState<VoteListProp[]>([])
+
+  const [value, setValue] = useState(1)
+
+  const onChange = (e: RadioChangeEvent) => setValue(e.target.value)
+
+  const handleConfirm = () => {
+    console.log(value)
   }
+
   useEffect(() => {
+    const getVoteList = async () => {
+      const provider = new ethers.JsonRpcProvider('http://0.0.0.0:8545')
+      const contractAddresss = '0xce30F4Bba32e48bAe66a9011Fe947A156f549018'
+      const abi = [
+        {
+          constant: true,
+          inputs: [],
+          name: 'getVoteList',
+          outputs: [
+            {
+              components: [
+                {
+                  internalType: 'bytes32',
+                  name: 'name',
+                  type: 'bytes32'
+                },
+                {
+                  internalType: 'uint256',
+                  name: 'voteCount',
+                  type: 'uint256'
+                }
+              ],
+              internalType: 'struct Ballot.Proposal[]',
+              name: '',
+              type: 'tuple[]'
+            }
+          ],
+          payable: false,
+          stateMutability: 'view',
+          type: 'function'
+        }
+      ]
+      const contract = new ethers.Contract(contractAddresss, abi, provider)
+      console.dir(contract.chairperson)
+      let voteList = await contract.getVoteList()
+      voteList = [...voteList].map((i) => i[0])
+      voteList = voteList.map((i: string) => ({ label: ethers.decodeBytes32String(i), value: i }))
+      setVoteList(voteList)
+    }
     getVoteList()
   }, [])
   return (
-    <div className="w-min-[600px] m-auto mt-5  shadow-lg p-4">
+    <div className="w-[400px] m-auto mt-[10%]  shadow-lg p-4">
       <h2 className="text-[26px]">Vote List</h2>
-      <ul className="vote-list">
-        {voteList.map((i) => (
-          <li key={i}>{i}</li>
-        ))}
-      </ul>
+      <Radio.Group className="flex flex-col" onChange={onChange} value={value}>
+        {voteList.length > 0 &&
+          voteList.map((i) => (
+            <Radio className="mt-2" key={i.label} value={i.value}>
+              {i.label}
+            </Radio>
+          ))}
+      </Radio.Group>
+      <div className="footer mt-4 flex justify-end">
+        <Button type="primary" onClick={handleConfirm}>
+          confirm
+        </Button>
+      </div>
     </div>
   )
 }
---
+++ src/pages/Home/interface.d.ts
@@ -0,0 +1,4 @@
+export interface VoteListProp {
+  value: string
+  label: string
+}
